c4205a725a5651a42fa8d2b6c628e30e95b55150
mike-griffin <mike_griffin@comcast.net>
2021-12-14 20:10:55 -0800

    Added connect with strava button

--- FitViz-iOS/Views/SettingsViewController.swift
+++ FitViz-iOS/Views/SettingsViewController.swift
@@ -8,7 +8,30 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
+    let connectStravaButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Connect with Strava", for: .normal)
+        button.backgroundColor = .systemBlue
+        button.setTitleColor(.label, for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
-        view.backgroundColor = .systemTeal
+        view.backgroundColor = .systemBackground
+        configureSubviews()
+        configureConstraints()
+    }
+    
+    // MARK: UI Setup Functions
+    private func configureSubviews() {
+        view.addSubview(connectStravaButton)
+    }
+    
+    private func configureConstraints() {
+        NSLayoutConstraint.activate([
+            connectStravaButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            connectStravaButton.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor)
+        ])
     }
 }
